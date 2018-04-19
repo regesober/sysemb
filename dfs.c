194df7ccea2153d7696607e8e0c6769c455e057d
Reges Oberderfer <regesober@gmail.com>
2018-04-19 20:20:53 -0300

    added 3 algorithms

---
+++ dfs.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo_t {
+    int left;
+    int right;
+    int depth;
+} Node;
+
+Node* createBinaryTree (int nLevels) {
+    int numberOfNodes = (1 << (nLevels + 1)) - 1;
+    Node* bTree = (Node*) malloc(numberOfNodes*sizeof(Node));
+    int i, j;
+    int ref = 0;
+    int counter = 0;
+    for (i = 0; i < nLevels - 1; i++) {
+        for (j = ref; j < ref + (1 << i); j++) {
+            bTree[j].left = ++counter;
+            bTree[j].right = ++counter;
+        }
+        ref = ref + (1 << i);
+    }
+    for (i = ref; i < numberOfNodes; i++) {
+        bTree[i].left = -1;
+        bTree[i].right = -1;
+    }
+    return bTree;
+}
+
+void calculateDepth_r (Node* bTree, int node, int depth) {
+    bTree[node].depth = depth;
+    if (bTree[node].left != -1)
+        calculateDepth_r(bTree, bTree[node].left, ++depth);
+    if (bTree[node].right != -1)
+        calculateDepth_r(bTree, bTree[node].right, depth);
+}
+
+void calculateDepth (Node* bTree) {
+    calculateDepth_r(bTree, 0, 0);
+}
+
+int main(int argc, char** argv) {
+    int nLevels = 6;
+    Node* bTree = createBinaryTree(nLevels);
+    calculateDepth(bTree);
+    return (EXIT_SUCCESS);
+}
+
