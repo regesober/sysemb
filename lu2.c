0f6b0a2c4d443f2857f937fcaeea93ffd0b83a81
Réges Oberderfer <regesober@gmail.com>
2018-04-20 15:58:18 -0300

    added a new lu algorithm that does not use define

---
+++ lu2.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+int Doolittle_LU_Decomposition(double *A, int n)
+{
+   int i, j, k, p;
+   double *p_k, *p_row, *p_col;
+
+//         For each row and column, k = 0, ..., n-1,
+//            find the upper triangular matrix elements for row k
+//            and if the matrix is non-singular (nonzero diagonal element).
+//            find the lower triangular matrix elements for column k. 
+ 
+   for (k = 0, p_k = A; k < n; p_k += n, k++) {
+      for (j = k; j < n; j++) {
+         for (p = 0, p_col = A; p < k; p_col += n,  p++)
+            *(p_k + j) -= *(p_k + p) * *(p_col + j);
+      }
+      if ( *(p_k + k) == 0.0 ) return -1;
+      for (i = k+1, p_row = p_k + n; i < n; p_row += n, i++) {
+         for (p = 0, p_col = A; p < k; p_col += n, p++)
+            *(p_row + k) -= *(p_row + p) * *(p_col + k);
+         *(p_row + k) /= *(p_k + k);
+      }  
+   }
+   return 0;
+}
+
+double* createMatrix (int n) {
+	double* A = (double*) malloc(n*n*sizeof(double));
+	int i, j;
+	for(i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			A[i*n + j] = 1;
+		}
+	}
+	return A;
+}
+
+int main(int argc, char** argv) {
+    int n = 1000;
+    double* A = createMatrix(n);
+	Doolittle_LU_Decomposition(A, n);
+    return (EXIT_SUCCESS);
+}
